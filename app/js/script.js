21954974d415a9e98522af8df4726686764e4581
rezarepo <reza@mediapuradigitalindonesia.com>
2021-01-24 20:10:27 +0700

    update home

---
+++ app/js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    $('.single-item').slick({
+        // autoplay: true,
+        // speed: 500,
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    draggable : true,
+                    dots: false,
+                    // autoplay: true,
+                    // speed: 500
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+      });
+    
+    $('.res-slide').slick({
+    dots: false,
+        infinite: false,
+        // speed: 300,
+        autoplay: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        draggable : false,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    draggable : true,
+                    autoplay: true,
+                    speed: 500
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+      
+    
+});
